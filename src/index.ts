27e8af812eaaa8a18d796c5d86e7df70c8cc5696
Nigtellios <shadowinkowal@gmail.com>
2025-12-08 00:39:40 +0100

    linter fixes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import app from './init/app';
+import app from "./init/app";
 
 /**
  * This is the "core" file for app, equivalent to "server" for many other templates.
  */
 
 export default {
-    port: 3137,
-    fetch: app.fetch,
-}
+  port: 3137,
+  fetch: app.fetch,
+};
