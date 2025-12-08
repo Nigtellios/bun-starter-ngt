161694498882a6aeae0f280f2629d86fd1babc7d
Nigtellios <shadowinkowal@gmail.com>
2025-12-08 22:10:46 +0100

    health check test linter

--- src/api/healthCheck/__tests__/healtCheck.test.ts
+++ src/api/healthCheck/__tests__/healtCheck.test.ts
@@ -1,17 +1,14 @@
-import { test, expect } from "bun:test";
-import { StatusCodes } from "http-status-codes";
+import { expect, test } from "bun:test";
 import type { ServiceResponse } from "@common/models/serviceResponse.ts";
 import app from "@init/app.ts";
+import { StatusCodes } from "http-status-codes";
 
-test(
-    "Health Check API endpoints",
-    async () => {
-        const response = await app.request("/health-check");
-        const result = (await response.json()) as ServiceResponse<null>;
-        
-        expect(response.status).toEqual(StatusCodes.OK);
-        expect(result.success).toBeTruthy();
-        expect(result.responseObject).toBeNull();
-        expect(result.message).toEqual("Service is healthy");
-    }
-);
+test("Health Check API endpoints", async () => {
+  const response = await app.request("/health-check");
+  const result = (await response.json()) as ServiceResponse<null>;
+
+  expect(response.status).toEqual(StatusCodes.OK);
+  expect(result.success).toBeTruthy();
+  expect(result.responseObject).toBeNull();
+  expect(result.message).toEqual("Service is healthy");
+});
