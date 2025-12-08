c99a8b330d51f2365ee9abf9b4553b172e55d6a4
Nigtellios <shadowinkowal@gmail.com>
2025-12-08 01:11:54 +0100

    useful links added to index

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,19 @@ import app from "./init/app";
 
 /**
  * This is the "core" file for app, equivalent to "server" for many other templates.
+ * 
+ * Useful docs:
+ * https://orm.drizzle.team/docs/get-started/postgresql-new
+ * https://bun.com/docs/bundler
+ * 
+ * https://hono.dev/docs/getting-started/bun
+ * https://hono.dev/examples/hono-openapi
+ * https://honohub.dev/docs/openapi/ui
+ * 
+ * https://zod.dev/basics
+ * https://biomejs.dev/guides/getting-started/
+ * 
+ * https://www.npmjs.com/package/http-status-codes
  */
 
 export default {
